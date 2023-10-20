8b97c4a9db7dce1803cf28921eb82534dec07ca1
edlymulyaandeslin4 <edlymulyaandeslin@gmail.com>
2023-10-20 21:48:58 +0700

    repair class

--- resources/views/dashboard/profile/display.blade.php
+++ resources/views/dashboard/profile/display.blade.php
@@ -1,24 +1,25 @@
 @extends('dashboard.layouts.main')
 @section('content')
-<main role="main">
-    <div class="d-flex flex-column flex-wrap flex-md-nowrap  pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">{{ auth()->user()->name }}</h1>
-    </div>
-
-    @if (session()->has('success'))
-    <div class="alert alert-success" role="alert">
-        {{ session('success') }}
-    </div>
-    @endif
-    <div class="jumbotron">
-        <div class="container text-center">
-            <h3 class="display-3">{{ auth()->user()->username }}</h3>
-            <p>Usia : {{ auth()->user()->umur }} tahun</p>
-            <p>Alamat : {{ auth()->user()->alamat }}</p>
-            <p>{{ auth()->user()->bio }}</p>
-            <p><a class="btn btn-dark btn-md" href="/dashboard/profile/{{ $profiles->id }}/edit" role="button">Change Profile &raquo;</a></p>
+    <main role="main">
+        <div class="d-flex flex-column flex-wrap flex-md-nowrap  pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">{{ auth()->user()->name }}</h1>
         </div>
-    </div>
-</main>
 
+        @if (session()->has('success'))
+            <div class="alert alert-success" role="alert">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        <div class="jumbotron">
+            <div class="container text-center">
+                <h3 class="display-3">{{ auth()->user()->username }}</h3>
+                <p>Usia : {{ auth()->user()->umur }} tahun</p>
+                <p>Alamat : {{ auth()->user()->alamat }}</p>
+                <p>{{ auth()->user()->bio }}</p>
+                <p><a class="btn btn-dark btn-md" href="/dashboard/profile/{{ $profiles->id }}/edit" role="button">Change
+                        Profile &raquo;</a></p>
+            </div>
+        </div>
+    </main>
 @endsection
--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -10,7 +10,7 @@
     <div class="d-flex flex-column flex-wrap flex-md-nowrap  pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Update Profile</h1>
     </div>
-    <div class="col-md-8 mb-3 ">
+    <div class="img col-md-8 mb-3 ">
         <form action="/dashboard/profile/{{ $profiles->id }}" method="POST" class="row g-3">
             @method('put')
             @csrf
